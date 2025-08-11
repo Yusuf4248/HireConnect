15ad95d6c329934e0ad3e340edc23993aefcedd1
Umidjon12346 <umidshari456@gmail.com>
2025-08-11 08:51:06 +0500

    swagger

--- src/hr_specialists/hr_specialists.controller.ts
+++ src/hr_specialists/hr_specialists.controller.ts
@@ -1,34 +1,109 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { HrSpecialistsService } from './hr_specialists.service';
-import { CreateHrSpecialistDto } from './dto/create-hr_specialist.dto';
-import { UpdateHrSpecialistDto } from './dto/update-hr_specialist.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { HrSpecialistsService } from "./hr_specialists.service";
+import { CreateHrSpecialistDto } from "./dto/create-hr_specialist.dto";
+import { UpdateHrSpecialistDto } from "./dto/update-hr_specialist.dto";
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiNotFoundResponse,
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+} from "@nestjs/swagger";
 
-@Controller('hr-specialists')
+@ApiTags("HR Specialists")
+@ApiBearerAuth() // If authentication is required
+@Controller("hr-specialists")
 export class HrSpecialistsController {
   constructor(private readonly hrSpecialistsService: HrSpecialistsService) {}
 
   @Post()
+  @ApiOperation({ summary: "Create a new HR specialist" })
+  @ApiCreatedResponse({
+    description: "The HR specialist has been successfully created",
+    type: CreateHrSpecialistDto,
+  })
+  @ApiBadRequestResponse({ description: "Invalid input data" })
+  @ApiBody({ type: CreateHrSpecialistDto })
   create(@Body() createHrSpecialistDto: CreateHrSpecialistDto) {
     return this.hrSpecialistsService.create(createHrSpecialistDto);
   }
 
   @Get()
+  @ApiOperation({ summary: "Get all HR specialists" })
+  @ApiOkResponse({
+    description: "List of all HR specialists",
+    type: [CreateHrSpecialistDto],
+    isArray: true,
+  })
   findAll() {
     return this.hrSpecialistsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  @ApiOperation({ summary: "Get HR specialist by ID" })
+  @ApiOkResponse({
+    description: "The HR specialist with the requested ID",
+    type: CreateHrSpecialistDto,
+  })
+  @ApiNotFoundResponse({ description: "HR specialist not found" })
+  @ApiParam({
+    name: "id",
+    type: Number,
+    description: "ID of the HR specialist to retrieve",
+    example: 1,
+  })
+  findOne(@Param("id") id: string) {
     return this.hrSpecialistsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateHrSpecialistDto: UpdateHrSpecialistDto) {
+  @Patch(":id")
+  @ApiOperation({ summary: "Update HR specialist information" })
+  @ApiOkResponse({
+    description: "The HR specialist has been successfully updated",
+    type: UpdateHrSpecialistDto,
+  })
+  @ApiNotFoundResponse({ description: "HR specialist not found" })
+  @ApiBadRequestResponse({ description: "Invalid input data" })
+  @ApiParam({
+    name: "id",
+    type: Number,
+    description: "ID of the HR specialist to update",
+    example: 1,
+  })
+  @ApiBody({ type: UpdateHrSpecialistDto })
+  update(
+    @Param("id") id: string,
+    @Body() updateHrSpecialistDto: UpdateHrSpecialistDto
+  ) {
     return this.hrSpecialistsService.update(+id, updateHrSpecialistDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  @ApiOperation({ summary: "Delete an HR specialist" })
+  @ApiOkResponse({
+    description: "The HR specialist has been successfully deleted",
+  })
+  @ApiNotFoundResponse({ description: "HR specialist not found" })
+  @ApiParam({
+    name: "id",
+    type: Number,
+    description: "ID of the HR specialist to delete",
+    example: 1,
+  })
+  remove(@Param("id") id: string) {
     return this.hrSpecialistsService.remove(+id);
   }
 }
--- src/job_seekers/job_seekers.controller.ts
+++ src/job_seekers/job_seekers.controller.ts
@@ -1,34 +1,105 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { JobSeekersService } from './job_seekers.service';
-import { CreateJobSeekerDto } from './dto/create-job_seeker.dto';
-import { UpdateJobSeekerDto } from './dto/update-job_seeker.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { JobSeekersService } from "./job_seekers.service";
+import { CreateJobSeekerDto } from "./dto/create-job_seeker.dto";
+import { UpdateJobSeekerDto } from "./dto/update-job_seeker.dto";
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiNotFoundResponse,
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+} from "@nestjs/swagger";
 
-@Controller('job-seekers')
+@ApiTags("Job Seekers")
+@ApiBearerAuth()
+@Controller("job-seekers")
 export class JobSeekersController {
   constructor(private readonly jobSeekersService: JobSeekersService) {}
 
   @Post()
+  @ApiOperation({ summary: "Create a new job seeker" })
+  @ApiCreatedResponse({
+    description: "The job seeker has been successfully created",
+    type: CreateJobSeekerDto,
+  })
+  @ApiBadRequestResponse({ description: "Invalid input data" })
+  @ApiBody({ type: CreateJobSeekerDto })
   create(@Body() createJobSeekerDto: CreateJobSeekerDto) {
     return this.jobSeekersService.create(createJobSeekerDto);
   }
 
   @Get()
+  @ApiOperation({ summary: "Get all job seekers" })
+  @ApiOkResponse({
+    description: "List of all job seekers",
+    type: [CreateJobSeekerDto],
+  })
   findAll() {
     return this.jobSeekersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  @ApiOperation({ summary: "Get a job seeker by ID" })
+  @ApiOkResponse({
+    description: "The job seeker with the requested ID",
+    type: CreateJobSeekerDto,
+  })
+  @ApiNotFoundResponse({ description: "Job seeker not found" })
+  @ApiParam({
+    name: "id",
+    type: "number",
+    description: "ID of the job seeker to retrieve",
+  })
+  findOne(@Param("id") id: string) {
     return this.jobSeekersService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateJobSeekerDto: UpdateJobSeekerDto) {
+  @Patch(":id")
+  @ApiOperation({ summary: "Update a job seeker" })
+  @ApiOkResponse({
+    description: "The job seeker has been successfully updated",
+    type: UpdateJobSeekerDto,
+  })
+  @ApiNotFoundResponse({ description: "Job seeker not found" })
+  @ApiBadRequestResponse({ description: "Invalid input data" })
+  @ApiParam({
+    name: "id",
+    type: "number",
+    description: "ID of the job seeker to update",
+  })
+  @ApiBody({ type: UpdateJobSeekerDto })
+  update(
+    @Param("id") id: string,
+    @Body() updateJobSeekerDto: UpdateJobSeekerDto
+  ) {
     return this.jobSeekersService.update(+id, updateJobSeekerDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  @ApiOperation({ summary: "Delete a job seeker" })
+  @ApiOkResponse({
+    description: "The job seeker has been successfully deleted",
+  })
+  @ApiNotFoundResponse({ description: "Job seeker not found" })
+  @ApiParam({
+    name: "id",
+    type: "number",
+    description: "ID of the job seeker to delete",
+  })
+  remove(@Param("id") id: string) {
     return this.jobSeekersService.remove(+id);
   }
 }
