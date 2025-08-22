9ce793de8b3aa13367405f4af4362cf163382d03
ubaydullayevadiyora <146558675+ubaydullayevadiyora@users.noreply.github.com>
2025-08-22 11:03:09 +0500

    errorfixed

--- src/filters/filters.module.ts
+++ src/filters/filters.module.ts
@@ -1,11 +1,13 @@
-// filters.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FiltersController } from './filters.controller';
 import { JobsFilterService } from './jobs-filter.service';
+import { Job } from '../jobs/entities/job.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Job])], 
   controllers: [FiltersController],
   providers: [JobsFilterService],
-  exports: [JobsFilterService], 
+  exports: [JobsFilterService],
 })
 export class FiltersModule {}
--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -4,12 +4,11 @@ import { JobsController } from './jobs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Job } from './entities/job.entity';
 import { JwtModule } from '@nestjs/jwt';
-import { JobsFilterService } from '../filters/jobs-filter.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Job]), JwtModule],
   controllers: [JobsController],
   providers: [JobsService],
-  exports: [JobsFilterService],
+  // exports: [JobsService],
 })
 export class JobsModule {}
